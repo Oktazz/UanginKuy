03c018b1444c947097dac191aa6367522f3541c0
Oktazz <oktadwi7611@gmail.com>
2026-07-29 09:42:51 +0800

    feat: add description field to RSS item interface for enhanced feed metadata parsing

--- src/utils/rss-fetcher.ts
+++ src/utils/rss-fetcher.ts
@@ -14,6 +14,7 @@ type CustomItem = {
   'media:content'?: { $: { url: string } };
   'media:thumbnail'?: { $: { url: string } };
   enclosure?: { url: string; type?: string };
+  description?: string;
 };
 
 const parser = new Parser<Record<string, unknown>, CustomItem>({
